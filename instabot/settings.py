a866b3d7d09c3d7d8ff7ae5d8baf064ef1afbebe
cloner <mrzjkb1375@gmail.com>
2023-05-07 18:18:21 +0330

    telegram added

--- instabot/settings.py
+++ instabot/settings.py
@@ -88,13 +88,9 @@ WSGI_APPLICATION = "instabot.wsgi.application"
 #     }
 # }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'instabot',
-        'USER': 'p7ayam',
-        'PASSWORD': '!LiL!Sor2087?P7@',
-        'HOST': '51.79.156.167',
-        'PORT': '3313',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
